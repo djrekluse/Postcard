e38b5179e1be66b68382406a51caaea0b85c7d06
Corey deVos <rekluse@Coreys-MacBook-Pro.local>
2014-09-28 19:22:41 -0600

    Test 2: Upload to GitHub

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
@@ -28,13 +29,19 @@ class ViewController: UIViewController {
 
   
     @IBAction func sendMailButtonPressed(sender: UIButton) {
-        // Evelyn Nyima deVos is AWESOME!
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
     
         mailButton.setTitle("Mail sent", forState: UIControlState.Normal)}
     
